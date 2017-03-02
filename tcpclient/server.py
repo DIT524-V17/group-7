c164d526f03fab2ae4a086ab3895a889bbd68994
Pontus <guslaepo@student.gu.se>
2017-03-02 10:51:54 +0100

    Server now handles disconnections properly

--- tcpclient/server.py
+++ tcpclient/server.py
@@ -1,3 +1,5 @@
+# Author: Pontus Laestadius.
+# Since: 1st March 2017.
 import socket
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    # create a socket object
@@ -12,10 +14,11 @@ serversocket.listen(5)
 while 1:
     clientsocket, addr = serversocket.accept()
     print("Got a connection from %s" % str(addr))
-
-    while (clientsocket.getsockname() != ""):
-        msg = 'Thank you for connecting' + "\r\n"
-        msg = clientsocket.recv(1024)  # Maximum amount of data to be sent.
-        print(msg.decode('ascii'))  # Messages received formatting.
+    try:
+        while clientsocket.getsockname() != "":
+            msg = clientsocket.recv(1024)  # Maximum amount of data to be sent.
+            print(msg.decode('ascii'))  # Messages received formatting.
+    except:
+        clientsocket.close()                        # Closes the connection if the client drops.
 
 
