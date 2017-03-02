53b8784981e46e32b6f4091d9f8146648018224b
Pontus <guslaepo@student.gu.se>
2017-03-02 11:47:20 +0100

    Improved structure.

--- tcpclient/client.py
+++ tcpclient/client.py
@@ -1,31 +1,32 @@
 # Author: Pontus Laestadius.
-# Since: 1st March 2017.
+# Since: 1st of March, 2017.
 import _thread
 import socket
-import sys
-import time
-
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # Define web socket.
 # s.connect(("192.168.0.148", 9010))                      # Connect to the raspberry pi.
-s.connect((socket.gethostname(), 9010))                      # Connect to the raspberry pi.
+s.connect((socket.gethostname(), 9010))                      # Connect to the local machine.
 
 
 def senddata():
+    s.send(("Connected").encode('ascii'))
     while 1:
-        userinput = input()
-        s.send(userinput.encode('ascii'))               # Sends input to server.
+        try:
+            s.send(input().encode('ascii'))               # Sends input to server.
+        except:
+            s.close()  # Closes the connection if the client drops.
+            _thread.exit()
+
 
 def receivedata():
     while 1:                                            # Infinite loop.
-        msg = s.recv(1024)                              # Maximum amount of data to be sent.
-        print(msg.decode('ascii'))                      # Messages received formatting.
-
-def pingserver():                                       # Pings the server every 5 seconds.
-    while 1:
-        s.send("!ping")                                 # The returning data will be handled by receivedata()
-        time.sleep(5)
+        try:
+            msg = s.recv(1024)                           # Maximum amount of data to be sent.
+            msg = msg.decode('ascii')
+            print(msg)
+        except:
+            s.close()                                        # Closes the connection if the client drops.
+            _thread.exit()
 
 _thread.start_new_thread(senddata(), ("Thread-SendData", 2, ))
 _thread.start_new_thread(receivedata(), ("Thread-ReceiveData", 3, ))
-_thread.start_new_thread(pingserver(), ("Thread-pingServer", 3, ))
--- tcpclient/server.py
+++ tcpclient/server.py
@@ -1,44 +1,40 @@
 # Author: Pontus Laestadius.
-# Since: 1st March 2017.
+# Since: 2nd of March, 2017.
 import socket
 import _thread
 import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    # create a socket object
+
 def senddata(cs):
+    cs.send(("Connected").encode('ascii'))
     while 1:
-        userinput = input()
-        cs.send(userinput.encode('ascii'))               # Sends input to server.
-    _thread.exit()
+        try:
+            cs.send(input().encode('ascii'))               # Sends input to server.
+        except:
+            cs.close()  # Closes the connection if the client drops.
+            _thread.exit()
+
 
 def receivedata(cs):
     while 1:                                            # Infinite loop.
         try:
-            msg = clientsocket.recv(1024)                           # Maximum amount of data to be sent.
+            msg = cs.recv(1024)                           # Maximum amount of data to be sent.
             msg = msg.decode('ascii')
             print(msg)
         except:
-            clientsocket.close()                                        # Closes the connection if the client drops.
-            c = 0
+            cs.close()                                        # Closes the connection if the client drops.
             _thread.exit()
-
-
 
 host = socket.gethostname()
 
 s.bind((host, 9010))                                     # bind to the port
 
 s.listen(5)                                              # queue up to 5 requests
-c = 0
+
 while 1:
     clientsocket, addr = s.accept()
     print("Got a connection from %s" % str(addr))
-
-    if c == 0:
-        _thread.start_new_thread(senddata, (clientsocket,))
-        _thread.start_new_thread(receivedata, (clientsocket,))
-        c = 1
-    else:
-        time.sleep(3)
-
-
+    _thread.start_new_thread(senddata, (clientsocket,))
+    _thread.start_new_thread(receivedata, (clientsocket,))
+    time.sleep(3)
