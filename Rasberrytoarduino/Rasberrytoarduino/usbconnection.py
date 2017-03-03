ca756dd6aea9ee3be369be2bbf3d989e36799b13
Unknown <gusjonati@student.gu.se>
2017-03-03 09:25:03 +0100

    Working versio for sending information

--- Rasberrytoarduino/Rasberrytoarduino/usbconnection.py
+++ Rasberrytoarduino/Rasberrytoarduino/usbconnection.py
@@ -1,7 +1,7 @@
 import serial
-arduino = serial.Serial('COM5', 9600, timeout=.1)
+arduino = serial.Serial('COM3', 9600, timeout=.1)
 while True:
-    byte = "kej"
+    byte = "kejQ"
     arduino.write(byte.encode())
     data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
     if data:
