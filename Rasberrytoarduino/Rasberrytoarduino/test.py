d325d730b596b4fd9cee524e699d662ef93babbd
Unknown <gusjonati@student.gu.se>
2017-03-03 09:28:04 +0100

    Added the old version instead of the new one

---
+++ Rasberrytoarduino/Rasberrytoarduino/test.py
@@ -0,0 +1,22 @@
+import serial, time
+arduino = serial.Serial('COM3', 9600, timeout=.1)
+time.sleep(1) #give the connection a second to settle
+arduino.write("Hello world".encode())
+data = arduino.readline()
+if data:
+	print(data)
+
+arduino.write("1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye 1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world1 Bye cruel world 2 Bye cruel world 3 Bye cruel world 4 Bye cruel world 5 Bye cruel world 6 Bye cruel world99".encode())
+data = arduino.readline()
+if data:
+	print(data)
+
+arduino.write("Hello world".encode())
+data = arduino.readline()
+if data:
+	print(data)
+
+arduino.write("Hello world".encode())
+data = arduino.readline()
+if data:
+	print(data)
--- Rasberrytoarduino/Rasberrytoarduino/usbconnection.py
+++
@@ -1,8 +0,0 @@
-import serial
-arduino = serial.Serial('COM3', 9600, timeout=.1)
-while True:
-    byte = "kejQ"
-    arduino.write(byte.encode())
-    data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
-    if data:
-        print(data)
