7d2e8860fa372993aa47f8c8f36a60b5f217bd39
Sebastian Fransson <gusfransse@student.gu.se>
2017-03-23 14:25:49 +0100

    Updated manifest and quick AsyncTask 'integration'.
    Updated the Android manifest for Wi-fi connection, Implemented a quickly made AsyncTask that will allow for sub-threading.

---
+++ F.R.O.S.T/app/src/main/java/t/s/o/r/f/frost/Client.java
@@ -0,0 +1,157 @@
+package t.s.o.r.f.frost;// File Name GreetingClient.java
+import java.net.*;
+import java.io.*;
+
+import java.net.*;
+import java.io.*;
+
+import java.util.*;
+
+public class Client {
+
+	static int port = 9005;
+	static int port2 = 9000;
+	static String host = "192.168.0.120";
+
+	public static void main(String [] args) {
+
+		/*
+		try {
+			host = InetAddress.getLocalHost().getHostName(); // TODO: 07/03/2017 replace with raspberry pi ip when implementing.
+		} catch (UnknownHostException e){
+			System.out.print("Unkown host");
+		}
+		*/
+
+		Transmitter r1 = initTransmitter(host, port);
+		// Receiver r2 = initReceiver(host, port2);
+
+		// TODO: 05/03/2017 currently always keeps the client alive. Replace with automatic reconnection.
+	}
+
+	public static Transmitter initTransmitter(String host, int port){
+		Transmitter R1 = new Transmitter(host, port);
+		R1.start();
+		return R1;
+	}
+
+	public static Receiver initReceiver(String host, int port){
+		Receiver R1 = new Receiver(host, port);
+		R1.start();
+		return R1;
+	}
+}
+
+class BaseSocket implements Runnable {
+	private Thread t;
+	private String host;
+	private int port;
+	Queue<String> input = new PriorityQueue<>();
+	Socket socket;
+
+	// Macro for add.
+	public void write(String s){
+		// TODO: 06/03/2017 Add command vertification here. Preferably O(1).
+		input.add(s);
+	}
+
+	BaseSocket(String host, int port) {
+		this.host = host;
+		this.port = port;
+	}
+
+	public void run() {
+	}
+
+	static void p(String s){
+		System.out.println(s);
+	}
+
+	void start () {
+		if (t == null) {
+			t = new Thread (this);
+			t.start ();
+		}
+	}
+}
+
+class Transmitter extends BaseSocket implements Runnable {
+	private Thread t;
+
+	Transmitter(String host, int port) {
+		super(host,port);
+		try {
+			socket = new Socket(host, port);
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		write("cc"); // Enables the receiving socket. Do not remove.
+
+		try {
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			p(this.getClass().toString() + " online.");
+
+			try {
+				while (!socket.isInputShutdown()) { // Checks if the socket is able to receive data.
+					if (!input.isEmpty()){ // Checks if the st4ack has any commands in it waiting.
+						String s = input.poll();
+						out.writeUTF(s); // TODO: 05/03/2017 Needs to be replaced for android input.
+					}
+				}
+			} finally {
+				p("Abandon Transmitter thread, It's going down!");
+			}
+
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+
+		p(this.getClass().toString() + " exiting.");
+	}
+
+	void start () {
+		super.start();
+	}
+}
+
+class Receiver extends BaseSocket implements Runnable {
+	private Thread t;
+	private ServerSocket ssocket;
+
+	Receiver(String host, int port) {
+		super(host,port);
+		try {
+			ssocket = new ServerSocket(port);
+			ssocket.setSoTimeout(10000);
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+
+		try {
+			socket = ssocket.accept();
+			DataInputStream in = new DataInputStream(socket.getInputStream());
+			p(this.getClass().toString() + " online.");
+
+			while (true){
+				p(in.readUTF());
+			}
+
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+
+		p(this.getClass().toString() + " exiting.");
+	}
+
+	void start () {
+		super.start();
+	}
+}
--- F.R.O.S.T/app/src/main/java/t/s/o/r/f/frost/MainActivity.java
+++ F.R.O.S.T/app/src/main/java/t/s/o/r/f/frost/MainActivity.java
@@ -1,5 +1,7 @@
 package t.s.o.r.f.frost;
 
+import android.os.AsyncTask;
+import android.os.NetworkOnMainThreadException;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,100 +11,146 @@ import android.view.View;
 import android.widget.Button;
 import android.view.View.OnTouchListener;
 
-import java.util.MissingResourceException;
+import java.io.IOException;
+import java.util.Scanner;
+
+import static t.s.o.r.f.frost.Client.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    //static String drive = "d070";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button forward = (Button)findViewById(R.id.button);
+        Button forward = (Button) findViewById(R.id.button);
+        new threads().execute();
+
+       // final Transmitter r1 = initTransmitter(host, port);
+        //final Transmitter r2 = initTransmitter(host, port2);
         //Sets the TouchListener to 'button' which in this case refers to the *FORWARD* button. (Check XML).
-        forward.setOnTouchListener(new View.OnTouchListener(){
-            @Override
-            public boolean onTouch(View v, MotionEvent event){
-                try {
-                    //Sends a "forward" commmand to the Raspberry pi (to be integrated)
-                    if(event.getAction() == MotionEvent.ACTION_DOWN) {
-                        System.out.println("Drive forward");
+        forward.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    try {
+                        //Sends a "forward" commmand to the Raspberry pi (to be integrated)
+                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+                            threads.r1.write("d070?");
+
+
+                            // System.out.println("Drive forward");
+                        }
+                        //Sends command to stop the current activity (to be integrated)
+                        if (event.getAction() == MotionEvent.ACTION_UP) {
+                            threads.r1.write("d090?");
+                            //System.out.println("stop driving");
+                        }
+                    } catch (Exception e) {
+                        System.out.println("MAH GOD WHY");
+                        e.printStackTrace();
                     }
-                    //Sends command to stop the current activity (to be integrated)
-                    if(event.getAction() == MotionEvent.ACTION_UP){
-                        System.out.println("stop driving");
-                    }
-                }catch(Exception e){
-                    System.out.println("MAH GOD WHY");
+                    return false;
                 }
-                return false;
-            }
-        });
+            });
 
 
-        Button reverse= (Button)findViewById(R.id.button2);
-        //Sets the TouchListener to 'button2' which in this case refers to the *Reverse* button. (Check XML).
-        reverse.setOnTouchListener(new View.OnTouchListener(){
-            @Override
-            public boolean onTouch(View v, MotionEvent event){
-                try {
-                    //Sends a "reverse" commmand to the Raspberry pi (to be integrated)
-                    if(event.getAction() == MotionEvent.ACTION_DOWN) {
-                        System.out.println("Reversing");
+            Button reverse = (Button) findViewById(R.id.button2);
+            //Sets the TouchListener to 'button2' which in this case refers to the *Reverse* button. (Check XML).
+            reverse.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    try {
+                        //Sends a "reverse" commmand to the Raspberry pi (to be integrated)
+                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+                            threads.r1.write("d110?");
+                            // System.out.println("Reversing");
+                        }
+                        //Sends command to stop reversing (to be integrated)
+                        if (event.getAction() == MotionEvent.ACTION_UP) {
+                            threads.r1.write("d090?");
+                            //  System.out.println("Stop reversing");
+                        }
+                    } catch (Exception e) {
+                        System.out.println("MAH GOD WHY");
+                        e.printStackTrace();
                     }
-                    //Sends command to stop reversing (to be integrated)
-                    if(event.getAction() == MotionEvent.ACTION_UP){
-                        System.out.println("Stop reversing");
-                    }
-                }catch(Exception e){
-                    System.out.println("MAH GOD WHY");
+                    return false;
                 }
-                return false;
-            }
-        });
+            });
 
 
-        Button rightSteer= (Button)findViewById(R.id.button4);
-        //Sets the TouchListener to 'button4' which in this case refers to the *Right Steer* button. (Check XML).
-        rightSteer.setOnTouchListener(new View.OnTouchListener(){
-            @Override
-            public boolean onTouch(View v, MotionEvent event){
-                try {
-                    //Sends a "go right" commmand to the Raspberry pi (to be integrated)
-                    if(event.getAction() == MotionEvent.ACTION_DOWN) {
-                        System.out.println("Steer Right");
+            Button rightSteer = (Button) findViewById(R.id.button4);
+            //Sets the TouchListener to 'button4' which in this case refers to the *Right Steer* button. (Check XML).
+            rightSteer.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    try {
+                        //Sends a "go right" commmand to the Raspberry pi (to be integrated)
+                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                            threads.r1.write("a090?");
+                            // System.out.println("Steer Right");
+                        }
+                        //Sends command to stop steering right (to be integrated)
+                        if (event.getAction() == MotionEvent.ACTION_UP) {
+                            threads.r1.write("a045?");
+                            // System.out.println("Stop steering right");
+                        }
+                    } catch (Exception e) {
+                        System.out.println("MAH GOD WHY");
+                        e.printStackTrace();
                     }
-                    //Sends command to stop steering right (to be integrated)
-                    if(event.getAction() == MotionEvent.ACTION_UP){
-                        System.out.println("Stop steering right");
-                    }
-                }catch(Exception e){
-                    System.out.println("MAH GOD WHY");
+                    return false;
                 }
-                return false;
-            }
-        });
+            });
 
-        Button leftSteer= (Button)findViewById(R.id.button3);
-        //Sets the TouchListener to 'button3' which in this case refers to the *Steer Left* button. (Check XML).
-        leftSteer.setOnTouchListener(new View.OnTouchListener(){
-            @Override
-            public boolean onTouch(View v, MotionEvent event){
-                try {
-                    //Sends a "go left" commmand to the Raspberry pi (to be integrated)
-                    if(event.getAction() == MotionEvent.ACTION_DOWN) {
-                        System.out.println("Steering Left");
+            Button leftSteer = (Button) findViewById(R.id.button3);
+            //Sets the TouchListener to 'button3' which in this case refers to the *Steer Left* button. (Check XML).
+            leftSteer.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    try {
+                        //Sends a "go left" commmand to the Raspberry pi (to be integrated)
+                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                            threads.r1.write("a000?");
+                            //  System.out.println("Steering Left");
+                        }
+                        //Sends command to stop steering left (to be integrated)
+                        if (event.getAction() == MotionEvent.ACTION_UP) {
+                            threads.r1.write("a045?");
+                            // System.out.println("Stop steering left");
+                        }
+                    } catch (Exception e) {
+                        System.out.println("MAH GOD WHY");
+                        e.printStackTrace();
                     }
-                    //Sends command to stop steering left (to be integrated)
-                    if(event.getAction() == MotionEvent.ACTION_UP){
-                        System.out.println("Stop steering left");
-                    }
-                }catch(Exception e){
-                    System.out.println("MAH GOD WHY");
+                    return false;
                 }
-                return false;
-            }
-        });
+            });
     }
+
+
+   public static class threads extends AsyncTask<String, Void, Void> {
+       static Transmitter r1;
+        @Override
+        public Void doInBackground(String... params) {
+
+            try {
+                r1 = initTransmitter(host, port);
+
+
+            } catch (Exception e) {
+                System.out.println("Screw you");
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+
     /*public void forwardTest(View view){
         Button Forward = (Button) view;
         ((Button) view).setText("Does this happen?");
