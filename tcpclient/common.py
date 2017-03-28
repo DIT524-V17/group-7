f6788d67649265343089821949e615ace81fd91e
Pontus <guslaepo@student.gu.se>
2017-03-28 11:51:46 +0200

    Updated

--- tcpclient/common.py
+++ tcpclient/common.py
@@ -29,47 +29,35 @@ class Receiver(Thread):
     def run(self):
 
         # binds the server to the port.
-        print("Binding.")
         self.receiver.bind((self.host, self.port))
-
-        print("Listening.")
 
         # Listens for packages of size 5. Any other size causes artifacts.
         self.receiver.listen(5)
 
-        print("Looping.")
-
         while 1:
-
             (client, address) = self.receiver.accept()
             print(client.getsockname)
             if client.getsockname() != "":
                 print("Receiver online")
                 break
         while self.host != "":
-            try:
-                msg = client.recv(1024)
-                if not msg:
-                    self.disconnected("receivedata(): peer disconnected")
-                if not self.connection:
-                    self.connection = True
-                    continue
-                msg = msg.decode(textconverter)
-                print(msg)
+            msg = client.recv(1024)
+            if not msg:
+                self.disconnected("because peer disconnected")
+            if not self.connection:
+                self.connection = True
+                continue
+            msg = msg.decode(textconverter)
+            print(msg)
 
-                # Tim's Added line for sending the commands to the Arduino
-                usbconnection.write(msg.encode())
+            # Tim's Added line for sending the commands to the Arduino
+            usbconnection.write(msg.encode())
 
-                # If the message
-            except:
-                self.disconnected("Exception caught")
 
     # Handles errors.
     def disconnected(self, s):
-        print("Disconnected at: %s" % s)
-        self.host = ""
-        self.connection = False
-        self.receiver = ""
+        print("Disconnected %s" % s)
+        sys.exit(1)
 
 
 class Transmitter(Thread):
@@ -78,6 +66,7 @@ class Transmitter(Thread):
     host = ""
     port = 0
 
+    # Constructor
     def __init__(self, host, port):
         Thread.__init__(self)
         self.host = host
@@ -85,6 +74,7 @@ class Transmitter(Thread):
         self.daemon = True
         self.start()
 
+    # On start
     def run(self):
         attempts = 0
         while 1:
@@ -97,17 +87,23 @@ class Transmitter(Thread):
                 self.transmitter.send("cc".encode(textconverter))
                 break
             except:
+
+                # Tries to connect up to 5 times before giving up.
                 attempts += 1
                 if attempts < 6:
                     print("#%s Attempting to connect. " % attempts)
                 else:
-                    print("Couldn't connect.")
-                    del self
+                    self.disconnected("because I couldn't connect")
+
         print("Transmitter online")
 
         while 1:
             try:
                 self.transmitter.send(input().encode(textconverter))
             except OSError:
-                Receiver.disconnected(self, "senddata()")
-                del self
+                self.disconnected("due to server")
+
+    # Handles errors.
+    def disconnected(self, s):
+        print("Disconnected %s" % s)
+        sys.exit(1)
