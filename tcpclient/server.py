3fde18e7b6eec8921443f6a53f4f2b7df564b579
Pontus <guslaepo@student.gu.se>
2017-04-05 10:27:16 +0200

    Updated imports

--- tcpclient/server.py
+++ tcpclient/server.py
@@ -2,7 +2,7 @@
 # Since: 2nd of March, 2017.
 # Maintained since: 4th of April 2017.
 from common import Receiver
-from common import Transmitter
+
 
 def init():
     Receiver("192.168.0.120", 9005)
