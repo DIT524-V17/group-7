edb80d29924424e0a5d382f6c46336da1d69b24b
Pontus <guslaepo@student.gu.se>
2017-04-07 18:50:00 +0200

    Removed a thread

--- tcpclient/common.py
+++ tcpclient/common.py
@@ -49,9 +49,11 @@ class Receiver(Thread):
         # Only breas when/if the client disconnects from the server.
         while self.connection:
 
-            Transmitter(client)
-
-            print("3")
+            try:
+                if usbconnection.readline():
+                    client.send(usbconnection.readline().decode().encode(textconverter))
+            except BrokenPipeError:
+                break
 
             # Receives up to 1024 bytes I think. Do some more reserach on teh purpose of this.
             msg = client.recv(1024)
@@ -76,24 +78,3 @@ class Receiver(Thread):
     @staticmethod
     def disconnected(s):
         print("Disconnected at: %s" % s)
-
-class Transmitter(Thread):
-
-    s = socket
-
-    def __init__(self, ssocket):
-        Thread.__init__(self)
-        self.s = ssocket
-        self.daemon = True
-        self.start()
-
-    # On thread run.
-    def run(self):
-        while 1:
-
-            # Reads from the Serial and sends it to the client.
-            try:
-                if usbconnection.readline():
-                    self.s.send(usbconnection.readline().decode().encode(textconverter))
-            except:
-                raise
