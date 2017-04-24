8deed585ae62203bc70765f93b5a73e0226bc792
IsabelleTornqvist <gustornqis@student.gu.se>
2017-04-24 12:35:22 +0200

    Switching sensors on and off (#13)
    * Added temperature sensor code to arduino main sketch
    
    * Added delay when sending temperature.
    
    +Added delay when sending temperature. 
    +Allowing for "switching off" sensors, work in progress
    
    * Added delay when sending temperature.
    
    * added a comment to newPing
    
    * Allow for turning sensors on and off
    
    * Updated changes
    
    * Fixed minor errors
    
    * Fixed comments vol2
    
    * Fixed comments vol3
    
    * Fixed line 218 that is always wrong

--- SmartCar/Arduino/sketch_main/sketch_main.ino
+++ SmartCar/Arduino/sketch_main/sketch_main.ino
@@ -1,6 +1,11 @@
-#include <NewPing.h>
+#include <NewPing.h> // https://bitbucket.org/teckel12/arduino-new-ping/downloads/
 #include <Servo.h>
+//Library. Callibrates digital temperature readings.
+#include <OneWire.h> //https://github.com/PaulStoffregen/OneWire  
+//Library derived from OneWire. Used to do calculations regarding the temperature sensor. 
+#include <DallasTemperature.h> //https://github.com/milesburton/Arduino-Temperature-Control-Library 
 
+#define ONE_WIRE_BUS 3 //for temperature sensor
 #define ECHO_PIN 2
 #define TRIG_PIN 4
 #define STEER_PIN 5
@@ -16,19 +21,32 @@
 #define MAX_DISTANCE_ULTRASONIC 400
 
 int flame_pin;
+
 int velocity;
 int range;
+//Temperature variables
+int tempC;
+int oldTemp;
 
 int collisionServoPosition = START_POSITION_STEER_SERVO;
 int collision_delay = 250;
 int flame_delay = 500;
 int lastDistance = 0;
 int distance_Delay = 0;
+int tempDelay = 0;
 
 boolean collisionDirection;
 boolean obstacle;
 boolean completeCommand;
 boolean flame;
+
+//For turning modules on and off
+boolean flameActivation = true;
+boolean temperatureActivation = true;
+boolean ultrasonicActivation = true; 
+boolean servoActivation = true;     
+boolean motorActivation = true;
+boolean allActivation = true; //Used for de-activation of all sensors/modules 
 
 Servo motor;
 Servo steer;
@@ -41,6 +59,14 @@ char data;
 
 NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE_ULTRASONIC);
 
+//Setup a oneWire, used for temperature sensor
+OneWire oneWire(ONE_WIRE_BUS);
+
+//Library used for temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+// array to hold device readings
+DeviceAddress thermometer;
 
 void setup() {
     //Sets the motor and steering servos to the default positions
@@ -57,16 +83,48 @@ void setup() {
     pinMode(FLAME_SENSOR_PIN, INPUT);
     
     Serial.begin(9600);  
+
+  // Starting up <DallasTemperature.h>
+    sensors.begin();    
+    if (!sensors.getAddress(thermometer, 0));  
+    
+}
+
+//Added by Isabelle Törnqvist 2017-04-18
+// function to print the temperature for a device
+void printTemperature(DeviceAddress deviceAddress)
+{  
+  oldTemp = tempC;
+  tempC = (int)sensors.getTempC(deviceAddress);
+  String tempString;
+
+//Handling temperature: turning into a string of the appropriate length for sending to the Android app 
+  if(tempC < 10){
+    tempString = "t00" + (String)tempC;
+  }else if (tempC >= 100){
+    tempString = "t" + (String)tempC;
+  }
+  else{
+    tempString = "t0" + (String)tempC;
+  }
+
+//delay for sending the temperature: if the temperature has changed, send the temperature 
+  if(oldTemp != tempC){
+      Serial.println(tempString);   
+  }  
 }
 
 void loop() { 
     //Is true when a command with a ? in the end is given or if the input reaches a length atleast 4
     if(completeCommand){
+      
         switch(input.charAt(0)){
       
         //Controls the steering
         case 'a':
-            steer.write(input.substring(1, 4).toInt());
+            if (servoActivation){
+                steer.write(input.substring(1, 4).toInt());
+            }
             break;
       
         //Controls the speed
@@ -76,7 +134,38 @@ void loop() {
                 motor.write(velocity);
             }
             break;
-     
+        //case: '*' - switching sensors on or off, by setting bool variables to true or false. 
+        //input is sent from Android app. Checks first character - cases for all sensors/modules.   
+        case 'F':
+         flameActivation = !flameActivation;
+         break;
+
+        case 'T':
+          temperatureActivation = !temperatureActivation;
+          break;
+
+        case 'U':
+          ultrasonicActivation = !ultrasonicActivation;
+          break;
+
+        case 'S':
+          servoActivation =!servoActivation;
+          break;
+
+        case 'M':
+          motorActivation = !motorActivation; 
+          break;
+
+        //switch off all sensors
+        case 'E':
+          allActivation = !allActivation;
+          motorActivation = allActivation;
+          servoActivation = allActivation;
+          ultrasonicActivation = allActivation;
+          temperatureActivation = allActivation;
+          flameActivation = allActivation;        
+          break;       
+      
         default:
             motor.write(START_POSITION_MOTOR_SERVO);
             steer.write(START_POSITION_STEER_SERVO);
@@ -87,25 +176,32 @@ void loop() {
         completeCommand = false;
     }
 
-    //Reads the value from the flame sensor
-    flame_pin = digitalRead (FLAME_SENSOR_PIN) ;
+//Activation of the flame-sensor: if flameActivation is false, this if-statement won't be executed. 
+//This is were the detection of flame is done
+    if(flameActivation){
+      //Reads the value from the flame sensor
+      flame_pin = digitalRead (FLAME_SENSOR_PIN) ;
 
-    //If the flame sensor sees a flame it writes a command that says it sees a flame
-    if (flame_pin == HIGH && !flame){
-      if (++flame_delay >= 500){
+      //If the flame sensor sees a flame it writes a command that says it sees a flame
+      if (flame_pin == HIGH && !flame){
+        if (++flame_delay >= 500){
           Serial.write("f001\n");
-      }
-      flame = true;
-      digitalWrite(LED_PIN, HIGH);
+        }
+        flame = true;
+        digitalWrite(LED_PIN, HIGH);
 
-    //when the flame disapears it sends a command that it stopped seing a flame
-    }else if(flame && flame_pin == LOW){
-      Serial.write("f000\n");
-      flame = false;
-      digitalWrite(LED_PIN, LOW);
-      flame_delay = 0;
-    }
-    //Pings the ultrasonic sensor
+       //when the flame disapears it sends a command that it stopped seing a flame
+      }else if(flame && flame_pin == LOW){
+        Serial.write("f000\n");
+        flame = false;
+        digitalWrite(LED_PIN, LOW);
+        flame_delay = 0;
+      }  
+  }
+
+//Activation of ultrasonic sensor
+  if(ultrasonicActivation){
+   //Pings the ultrasonic sensor
     range_string = sonar.ping_cm();
     range = range_string.toInt();
     range_string = "c" + range_string + '\n';
@@ -114,10 +210,12 @@ void loop() {
         Serial.print(range_string);
         lastDistance = range;
         distance_Delay = 0;
-    }
+    }   
+  }
 
-    //Allows the car to drive backwards if an obstacle is in from of the car
-    if (velocity < 90){
+//Activation of motor
+//Allows the car to drive backwards if an obstacle is in from of the car
+if (motorActivation && velocity < 90 ){
       //Is true if the range is less than 50 cm or if the flame sensor has set the flame variable to true
         if (((range != 0 && range <= 50) && ++collision_delay >= 250) || flame){
             if (range > 20){
@@ -136,6 +234,12 @@ void loop() {
             motor.write(START_POSITION_MOTOR_SERVO);
         }
     }
+
+//Activation for temperature sensor
+if(temperatureActivation){
+    sensors.requestTemperatures(); // Get temperatures
+    printTemperature(thermometer); //Call 
+    }   
 }
 
 void serialEvent() {
