d934901cbabcacd759548e286e7bf51a8890e646
timjon <gusjonati@student.gu.se>
2017-04-28 18:03:23 +0200

    The code from the refactoring - Done on one computer but multiple contributors (#17)
    * Redesign of the arduino code
    
    * the version after the refactoring that doesnt work
    
    * It accually works

--- SmartCar/Arduino/sketch_main/sketch_main.ino
+++ SmartCar/Arduino/sketch_main/sketch_main.ino
@@ -5,65 +5,69 @@
 //Library derived from OneWire. Used to do calculations regarding the temperature sensor. 
 #include <DallasTemperature.h> //https://github.com/milesburton/Arduino-Temperature-Control-Library 
 
-#define ONE_WIRE_BUS 3 //for temperature sensor
-#define ECHO_PIN 2
-#define TRIG_PIN 4
-#define STEER_PIN 5
-#define FLAME_SENSOR_PIN 6 
-#define MOTOR_PIN 7
-#define COLLISION_SERVO_PIN 8
-#define LED_PIN 12
 
-#define START_POSITION_MOTOR_SERVO 90
-#define START_POSITION_STEER_SERVO 45
-#define START_POSITION_COLLISION_SERVO 50
+/*
+@Contributors
+Isabelle Tornqvist
+Tim Jonasson
+Pontus Laestadius
+Anthony Path
+*/
 
-#define MAX_DISTANCE_ULTRASONIC 400
+const int ONE_WIRE_BUS_PIN = 3; //for temperature sensor
+const int ECHO_PIN = 2;
+const int TRIG_PIN = 4;
+const int STEER_PIN = 5;
+const int FLAME_SENSOR_PIN = 6; 
+const int MOTOR_PIN = 7;
+const int COLLISION_SERVO_PIN = 8;
+const int LED_PIN = 12;
 
-int flame_pin;
+const int START_POSITION_MOTOR_SERVO = 90;
+const int START_POSITION_STEER_SERVO = 45;
+const int START_POSITION_COLLISION_SERVO = 50;
+
+const int MAX_DISTANCE_ULTRASONIC = 400;
+
+int flame_reading;
 
 int velocity;
-int range;
+int ultrasonic_range;
 //Temperature variables
-int tempC;
-int oldTemp;
+int temperature;
+int temperature_old;
 
-int collisionServoPosition = START_POSITION_STEER_SERVO;
 int collision_delay = 250;
 int flame_delay = 500;
-int lastDistance = 0;
-int distance_Delay = 0;
-int tempDelay = 0;
+int last_distance_read = 0;
+int distance_delay = 0;
+int temp_delay = 0;
 
-boolean collisionDirection;
-boolean obstacle;
-boolean completeCommand;
-boolean flame;
+boolean obstacle_detected;
+boolean complete_command;
+boolean flame_detected;
 
 //For turning modules on and off
-boolean flameActivation = true;
-boolean temperatureActivation = true;
-boolean ultrasonicActivation = true; 
-boolean servoActivation = true;     
-boolean motorActivation = true;
-boolean allActivation = true; //Used for de-activation of all sensors/modules 
+boolean flame_activation = true;
+boolean temperature_activation = true;
+boolean ultrasonic_activation = true; 
+boolean servo_activation = true;     
+boolean motor_activation = true;
+boolean all_activation = true; //Used for de-_activation of all sensors/modules 
 
 Servo motor;
 Servo steer;
-Servo collisionServo;
 
 String input;
-String range_string;
-
-char data;
+String ultrasonic_distance_string;
 
 NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE_ULTRASONIC);
 
 //Setup a oneWire, used for temperature sensor
-OneWire oneWire(ONE_WIRE_BUS);
+OneWire oneWire(ONE_WIRE_BUS_PIN);
 
 //Library used for temperature sensor 
-DallasTemperature sensors(&oneWire);
+DallasTemperature temperature_sensor(&oneWire);
 
 // array to hold device readings
 DeviceAddress thermometer;
@@ -74,23 +78,23 @@ DeviceAddress thermometer;
   * Author: Pontus Laestadius.
   * Since: 25th of April 2017
   */
-  Servo camera_servo_x;
-  Servo camera_servo_y;
-  const int Y_MAX_ANGLE = 100;
-  const int Y_MIN_ANGLE = 30;
+  Servo camera_servo_x_axis;
+  Servo camera_servo_y_axis;
+  const int Y_AXIS_MAX_ANGLE = 100;
+  const int Y_MIN_MAX_ANGLE = 30;
   
-  const int X_MAX_ANGLE = 180;
-  const int X_MIN_ANGLE = 0;
+  const int X_AXIS_MAX_ANGLE = 180;
+  const int X_MIN_MAX_ANGLE = 0;
   
-  int cameraX = 90; // Angle of the camera X
-  int cameraY = 90; // Angle of the camera Y
+  int camera_x = 90; // Angle of the camera X
+  int camera_y = 90; // Angle of the camera Y
   
   // See this picture as to why they are numbered this way:
   // http://pighixxx.com/megapdf.pdf
   const int CAMERA_PIN_X = 54; // Analog pin 0
   const int CAMERA_PIN_Y = 55; // Analog pin 1
-  int cameraX_valueReceivedFromTheRaspberry = 0; // Last X input.
-  int cameraY_valueReceivedFromTheRaspberry = 0; // Last Y input.
+  int camera_x_value_received_from_the_raspberry = 0; // Last X input.
+  int camera_y_value_received_from_the_raspberry = 0; // Last Y input.
 
 void setup() {
     //Sets the motor and steering servos to the default positions
@@ -99,52 +103,50 @@ void setup() {
     
     steer.attach(STEER_PIN);
     steer.write(START_POSITION_STEER_SERVO);
-    
-    collisionServo.attach(COLLISION_SERVO_PIN);
-    collisionServo.write(START_POSITION_COLLISION_SERVO);
-    
+
     pinMode(LED_PIN, OUTPUT);
     pinMode(FLAME_SENSOR_PIN, INPUT);
 
-    camera_servo_x.attach(CAMERA_PIN_X); //analog pin 0
-    camera_servo_y.attach(CAMERA_PIN_Y);  //analog pin 1
+    camera_servo_x_axis.attach(CAMERA_PIN_X); //analog pin 0
+    camera_servo_y_axis.attach(CAMERA_PIN_Y);  //analog pin 1
 
     // Resets the camera to a stationary position.
-    camera_servo_x.write(X_MAX_ANGLE/2);
-    camera_servo_y.write(Y_MAX_ANGLE/2);
+    camera_servo_x_axis.write(X_AXIS_MAX_ANGLE/2);
+    camera_servo_y_axis.write(Y_AXIS_MAX_ANGLE/2);
     
     Serial.begin(9600);  
 
   // Starting up <DallasTemperature.h>
-    sensors.begin();    
-    if (!sensors.getAddress(thermometer, 0));  
+    temperature_sensor.begin();    
+    if (!temperature_sensor.getAddress(thermometer, 0));  
     
 }
+/*
+  * Temperature sensor.
+  * Author: Isabelle Törnqvist.
+  * Since: 18th of April 2017
+  */
 
-//Added by Isabelle Törnqvist 2017-04-18
-// function to print the temperature for a device
-void printTemperature(DeviceAddress deviceAddress)
-{  
-  oldTemp = tempC;
-  tempC = (int)sensors.getTempC(deviceAddress);
+//function to print the temperature for a device
+void printTemperature(DeviceAddress deviceAddress){  
+  temperature_old = temperature;
+  temperature = (int)temperature_sensor.getTempC(deviceAddress);
   String tempString;
 
 //Handling temperature: turning into a string of the appropriate length for sending to the Android app 
-  if(tempC < 10){
-    tempString = "t00" + (String)tempC;
-  }else if (tempC >= 100){
-    tempString = "t" + (String)tempC;
-  }
-  else{
-    tempString = "t0" + (String)tempC;
+  if(temperature < 10){
+    tempString = "t00" + temperature;
+  }else if (temperature >= 100){
+    tempString = "t" + temperature;
+  }else{
+    tempString = "t0" + temperature;
   }
 
 //delay for sending the temperature: if the temperature has changed, send the temperature 
-  if(oldTemp != tempC){
+  if(temperature_old != temperature){
       Serial.println(tempString);   
   }  
 }
-
 
   /*
   * Camera servos and all related code. 
@@ -156,14 +158,12 @@ namespace camera {
   #define CAMERA_BOUNDRY(x,y,z) (x) < (y) ? (y) : (z)
   
   /*
-     Author: Pontus Laestadius
-     Since: 25th of April 2017
      Purpose: Handles the camera's servos with constraits.
      Do not call this method directly, Rather use setServoRelative.
   */
   int moveCamera(Servo &servo, const int &max, const int &min, int degrees) {
   
-    // If the valueReceivedFromTheRaspberrys exceed the angle the servo is able to turn.
+    // If the value_received_from_the_raspberrys exceed the angle the servo is able to turn.
     if (degrees < min || degrees > max) {
   
       // Which boundry does the camera exceed.
@@ -172,7 +172,7 @@ namespace camera {
       // Reset to either the minimum or maximum depending on which one it exceeds.
       servo.write(boundry);
   
-      // End the method. And return the valueReceivedFromTheRaspberry the camera angle is now set to.
+      // End the method. And return the value_received_from_the_raspberry the camera angle is now set to.
       return boundry;
     }
   
@@ -182,69 +182,60 @@ namespace camera {
   }
   
   /**
-     Author: Pontus Laestadius
-     Since: 25th of April 2017
-     Converts a valueReceivedFromTheRaspberry for the servo relative method.
+     Converts a value_received_from_the_raspberry for the servo relative method.
      Param: v is an integer. either 1 or 2 (everything else returns 0), that gets converted to -1 or 1.
   */
   int valueConverter(int v) {
     if (v == 1) { 
       return -v; // v = -1;
     } else if (v == 2) {
-      return  --v;
+      return  --v; // v = 1;
     }
     return 0;
   }
   
   /**
-     Author: Pontus Laestadius
-     Since: 25th of April 2017
      Moves the camera relative to the current position. Such as left, right, up and down.
-     Param: Servo to be moved, max and min angles the servo can move. valueReceivedFromTheRaspberry, either 1 for lowing the angle and 2 for increasing it.
+     Param: Servo to be moved, max and min angles the servo can move. value_received_from_the_raspberry, either 1 for lowing the angle and 2 for increasing it.
      Amplifier amplifies the input coordinates.
   */
   
-  void setServoRelative(Servo &servo, const int &max, const int &min, int valueReceivedFromTheRaspberry, int &currentAngle) {
+  void setServoRelative(Servo &servo, const int &max, const int &min, int value_received_from_the_raspberry, int &currentAngle) {
     // The servo will move in 2 degree increments.
     int amplifier = 2;
   
-    // Change the valueReceivedFromTheRaspberry for the camera angle to keep track of it.
-    currentAngle += valueConverter(valueReceivedFromTheRaspberry) * amplifier;
+    // Change the value_received_from_the_raspberry for the camera angle to keep track of it.
+    currentAngle += valueConverter(value_received_from_the_raspberry) * amplifier;
   
     // Call the method to set the angle of the servo.
     currentAngle = moveCamera(servo, max, min, currentAngle);
   } 
 
   /*
-   * Author: Pontus Laestadius.
-   * Since: 25th of April 2017
    * Updates the camera servos on each iteration, until a 0 is given.
    * Checks both x and y individually so that they both can be moved at once.
   */
   void updateServo(int &x, int &y) {
   
     if (x != 0) { 
-      setServoRelative(camera_servo_x, X_MAX_ANGLE, X_MIN_ANGLE, x, cameraX);
+      setServoRelative(camera_servo_x_axis, X_AXIS_MAX_ANGLE, X_MIN_MAX_ANGLE, x, camera_x);
     }
     if (y != 0) {
-      setServoRelative(camera_servo_y, Y_MAX_ANGLE, Y_MIN_ANGLE, y, cameraY);
+      setServoRelative(camera_servo_y_axis, Y_AXIS_MAX_ANGLE, Y_MIN_MAX_ANGLE, y, camera_y);
     }
   
   }
 }
 
-void loop() { 
-    //Is true when a command with a ? in the end is given or if the input reaches a length atleast 4
-    if(completeCommand){
-
+void command() {
       // Improves readability.
-      int valueReceivedFromTheRaspberry = input.substring(1, 4).toInt();
+      int value_received_from_the_raspberry = input.substring(1, 4).toInt();
       
         switch(input.charAt(0)){
       
         //Controls the steering
         case 'a':
-            if (servoActivation){
+            if (servo_activation){
                 steer.write(input.substring(1, 4).toInt());
             }
             break;
@@ -252,49 +243,49 @@ void loop() {
         //Controls the speed
         case 'd':
             velocity = input.substring(1, 4).toInt();
-            if ((!flame && !obstacle) || velocity >= 90){
+            if ((!flame_detected && !obstacle_detected) || velocity >= 90){
                 motor.write(velocity);
             }
             break;
         //case: '*' - switching sensors on or off, by setting bool variables to true or false. 
         //input is sent from Android app. Checks first character - cases for all sensors/modules.   
         case 'F':
-         flameActivation = !flameActivation;
+         flame_activation = !flame_activation;
          break;
 
         case 'T':
-          temperatureActivation = !temperatureActivation;
+          temperature_activation = !temperature_activation;
           break;
 
         case 'U':
-          ultrasonicActivation = !ultrasonicActivation;
+          ultrasonic_activation = !ultrasonic_activation;
           break;
 
         case 'S':
-          servoActivation =!servoActivation;
+          servo_activation =!servo_activation;
           break;
 
         case 'M':
-          motorActivation = !motorActivation; 
+          motor_activation = !motor_activation; 
           break;
 
         //switch off all sensors
         case 'E':
-          allActivation = !allActivation;
-          motorActivation = allActivation;
-          servoActivation = allActivation;
-          ultrasonicActivation = allActivation;
-          temperatureActivation = allActivation;
-          flameActivation = allActivation;        
+          all_activation = !all_activation;
+          motor_activation = all_activation;
+          servo_activation = all_activation;
+          ultrasonic_activation = all_activation;
+          temperature_activation = all_activation;
+          flame_activation = all_activation;        
           break;      
 
            // Camera servos,
       // Their input should fit this expression: (x||y)(0001||0002)
       case 'x': // Sets the command to be used in the update function for the camera.
-        cameraX_valueReceivedFromTheRaspberry = valueReceivedFromTheRaspberry;
+        camera_x_value_received_from_the_raspberry = value_received_from_the_raspberry;
         break;
       case 'y':
-        cameraY_valueReceivedFromTheRaspberry = valueReceivedFromTheRaspberry;
+        camera_y_value_received_from_the_raspberry = value_received_from_the_raspberry;
         break;
       
         default:
@@ -302,60 +293,76 @@ void loop() {
             steer.write(START_POSITION_STEER_SERVO);
             break;
         }
-        //Resets the it so the next command can be recorded
+        //Resets the it so the next command can be recorded 
         input = "";
-        completeCommand = false;
-    }
+        complete_command = false;
+    
+}
 
-    // Updates the camera servos angle.
-  camera::updateServo(cameraX_valueReceivedFromTheRaspberry, cameraY_valueReceivedFromTheRaspberry);
-
-//Activation of the flame-sensor: if flameActivation is false, this if-statement won't be executed. 
-//This is were the detection of flame is done
-    if(flameActivation){
-      //Reads the valueReceivedFromTheRaspberry from the flame sensor
-      flame_pin = digitalRead (FLAME_SENSOR_PIN) ;
+void readFlame(){
+//Reads the value_received_from_the_raspberry from the flame sensor
+      flame_reading = digitalRead (FLAME_SENSOR_PIN) ;
 
       //If the flame sensor sees a flame it writes a command that says it sees a flame
-      if (flame_pin == HIGH && !flame){
+      if (flame_reading == HIGH && !flame_detected){
         if (++flame_delay >= 500){
           Serial.write("f001\n");
         }
-        flame = true;
+        flame_detected = true;
         digitalWrite(LED_PIN, HIGH);
 
        //when the flame disapears it sends a command that it stopped seing a flame
-      }else if(flame && flame_pin == LOW){
+      }else if(flame_detected && flame_reading == LOW){
         Serial.write("f000\n");
-        flame = false;
+        flame_detected = false;
         digitalWrite(LED_PIN, LOW);
         flame_delay = 0;
       }  
-  }
 
-//Activation of ultrasonic sensor
-  if(ultrasonicActivation){
+}
+
+void readTemp(){
+    temperature_sensor.requestTemperatures(); // Get temperatures
+    printTemperature(thermometer); //Call  
+}
+
+void loop() { 
+     //Is true when a command with a ? in the end is given or if the input reaches a length atleast 4
+    if(complete_command){
+      command();
+    }
+    // Updates the camera servos angle.
+  camera::updateServo(camera_x_value_received_from_the_raspberry, camera_y_value_received_from_the_raspberry);
+
+//_activation of the flame-sensor: if flame_activation is false, this if-statement won't be executed. 
+//This is were the detection of flame is done
+    if(flame_activation){
+    readFlame();  
+    }
+
+//_activation of ultrasonic sensor
+  if(ultrasonic_activation){
    //Pings the ultrasonic sensor
-    range_string = sonar.ping_cm();
-    range = range_string.toInt();
-    range_string = "c" + range_string + '\n';
-    if (lastDistance != range && ++distance_Delay >= 100){
+    ultrasonic_distance_string = (String)sonar.ping_cm();
+    ultrasonic_range = ultrasonic_distance_string.toInt();
+    ultrasonic_distance_string = "c" + ultrasonic_distance_string + '\n';
+    if (last_distance_read != ultrasonic_range && ++distance_delay >= 100){
        // Serial.write(range);
-        Serial.print(range_string);
-        lastDistance = range;
-        distance_Delay = 0;
+        Serial.print(ultrasonic_distance_string);
+        last_distance_read = ultrasonic_range;
+        distance_delay = 0;
     }   
   }
 
-//Activation of motor
+//_activation of motor
 //Allows the car to drive backwards if an obstacle is in from of the car
-if (motorActivation && velocity < 90 ){
+if (motor_activation && velocity < 90 ){
       //Is true if the range is less than 50 cm or if the flame sensor has set the flame variable to true
-        if (((range != 0 && range <= 50) && ++collision_delay >= 250) || flame){
-            if (range > 20){
-                motor.write(velocity +(50 - range)/2);
+        if (((ultrasonic_range != 0 && ultrasonic_range <= 50) && ++collision_delay >= 250) || flame_detected){
+            if (ultrasonic_range > 20){
+                motor.write(velocity +(50 - ultrasonic_range)/2);
             } else {
-                obstacle = true;
+                obstacle_detected = true;
                 collision_delay = 0;
     
                 motor.write(START_POSITION_MOTOR_SERVO +30);
@@ -363,25 +370,27 @@ if (motorActivation && velocity < 90 ){
                 motor.write(START_POSITION_MOTOR_SERVO);
             }
         //Sets obstacle to false if the obstacle in front of the car disapeared
-        } else if (obstacle){
-            obstacle = false;
+        } else if (obstacle_detected){
+            obstacle_detected = false;
             motor.write(START_POSITION_MOTOR_SERVO);
         }
     }
 
-//Activation for temperature sensor
-if(temperatureActivation){
-    sensors.requestTemperatures(); // Get temperatures
-    printTemperature(thermometer); //Call 
-    }   
+//_activation for temperature sensor
+if(temperature_activation && ++temp_delay >= 1000){
+    readTemp();   
+    temp_delay = 0;
 }
-
+}
 
 
 void serialEvent() {
     //Records one command at a time when availible
+    
+    // String in = Serial.readStringUntil('?');
+    
     while(Serial.available()){
-        data = Serial.read();
+        char data = Serial.read();
       
         //Makes sure no artifacts are in the command
         if(data != '?' && isAlphaNumeric(data)){
@@ -389,7 +398,7 @@ void serialEvent() {
         }else if (input.length() >= 4){
         
             //Makes the loop do the command when the input is the end character ?
-            completeCommand = true;
+            complete_command = true;
             break;
         }
     }
