32f6786ce306428b26af970cb8ab972fac32ab11
Tim Jonasson <gusjonati@student.gu.se>
2017-05-08 11:38:50 +0200

    Just making a commit before i switch branch

--- SmartCar/Arduino/_5channeltest/_5channeltest.ino
+++ SmartCar/Arduino/_5channeltest/_5channeltest.ino
@@ -5,6 +5,9 @@ const int FLAME_SENSOR_3_PIN = 16;
 const int FLAME_SENSOR_4_PIN = 17;
 const int FLAME_SENSOR_5_PIN = 18;
 
+const int MAX_READING = 800;
+const int DONT_READ_ABOVE = 450;
+
 int analogValue_1 = 0;
 int analogValue_2 = 0;
 int analogValue_3 = 0;
@@ -22,6 +25,15 @@ void loop() {
     analogValue_3 = analogRead(FLAME_SENSOR_3_PIN);
     analogValue_4 = analogRead(FLAME_SENSOR_4_PIN);
     analogValue_5 = analogRead(FLAME_SENSOR_5_PIN);
+
+
+
+    if (analogValue_1 > MAX_READING
+        || analogValue_2 > MAX_READING
+        || analogValue_3 > MAX_READING
+        || analogValue_4 > MAX_READING
+        || analogValue_5 > MAX_READING){
+    }
 
     Serial.print(analogValue_1);   
     Serial.print(", ");
