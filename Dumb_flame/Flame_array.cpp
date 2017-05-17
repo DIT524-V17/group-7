ce32f213a16797afba26f2ebaba0b6fcee57b073
Tim Jonasson <gusjonati@student.gu.se>
2017-05-17 20:16:42 +0200

    This might accually work

---
+++ Dumb_flame/Flame_array.cpp
@@ -0,0 +1,88 @@
+//
+// Created by tim on 2017-05-12.
+//
+
+#include "Flame_array.h"
+
+Flame_array::Flame_array(int flame_pin) {
+    flame_array[0].setPin(flame_pin);
+    amountOfSensors = 1;
+}
+Flame_array::Flame_array(int flame_pin, int flame_pin_2) {
+    flame_array[0].setPin(flame_pin);
+    flame_array[1].setPin(flame_pin_2);
+    amountOfSensors = 2;
+}
+Flame_array::Flame_array(int flame_pin, int flame_pin_2, int flame_pin_3) {
+    flame_array[0].setPin(flame_pin);
+    flame_array[1].setPin(flame_pin_2);
+    flame_array[2].setPin(flame_pin_3);
+    amountOfSensors = 3;
+}
+Flame_array::Flame_array(int flame_pin, int flame_pin_2, int flame_pin_3, int flame_pin_4) {
+    flame_array[0].setPin(flame_pin);
+    flame_array[1].setPin(flame_pin_2);
+    flame_array[2].setPin(flame_pin_3);
+    flame_array[3].setPin(flame_pin_4);
+    amountOfSensors = 4;
+}
+Flame_array::Flame_array(int flame_pin, int flame_pin_2, int flame_pin_3, int flame_pin_4, int flame_pin_5) {
+    flame_array[0].setPin(flame_pin);
+    flame_array[1].setPin(flame_pin_2);
+    flame_array[2].setPin(flame_pin_3);
+    flame_array[3].setPin(flame_pin_4);
+    flame_array[4].setPin(flame_pin_5);
+    amountOfSensors = 5;
+}
+
+char* Flame_array::read_flame_array() {
+    (*(flame_array + index)).setFlame();
+
+    (*(flame_array + index)).getFlameValue() >= MAX_READING ? flame_sun = true : flame_sun = false;
+    if(!flame_sun) {
+        command[0] = 'f';
+        command[1] = '0';
+        command[2] =  '0' + index;
+        int flame_value = (*(flame_array + index)).getFlameValue();
+        if (compareToAverage(flame_value)) {
+            (*(flame_array + index)).setFlameBool(true);
+            command[3] = '1';
+            return command;
+        }
+        (*(flame_array + index)).setFlameBool(false);
+        command[3] = '0';
+        return command;
+    }
+    return sun;
+}
+
+bool Flame_array::compareToAverage(int &value){
+    average_value = 0;
+    int tempValue = value;
+    int i = 0;
+    for(i;i < amountOfSensors; i++){
+        average_value += flame_array[i].getFlameValue();
+    }
+    if(amountOfSensors > 1) {
+        average_value -= tempValue;
+        average_value = average_value / (amountOfSensors - 1);
+
+        if (tempValue > FLAME_SENSITIVITY + average_value) {
+            return true;
+        }
+    } else if (tempValue > FLAME_SENSITIVITY && tempValue < MAX_READING) {
+        return true;
+    }
+    return false;
+}
+
+char* Flame_array::read() {
+    if (flame_array[index].getPin() == 0 || index == 5){
+        this->index = 0;
+    }
+    flame_status = read_flame_array();
+    if(flame_status[1] != '1') {
+        this->index++;
+    }
+    return flame_status;
+}
--- Dumb_flame/Flame_array.h
+++ Dumb_flame/Flame_array.h
@@ -5,26 +5,21 @@
 #ifndef FLAME_FLAME_H
 #define FLAME_FLAME_H
 
-//#include <Arduino.h>
-#include <string>
-#include <iostream>
-
-using namespace std;
+#include <Arduino.h>
 
 class Flame{
 public:
     bool getFlameBool(){return flame_bool;}
     int  getFlameValue(){return flame_reading;}
     int  getPin(){return FLAME_SENSOR_PIN;}
-    void setFlame(){flame_reading = 500;}//analogRead(FLAME_SENSOR_PIN);}
+    void setFlame(){flame_reading = analogRead(FLAME_SENSOR_PIN);}
     void setFlame(int input){this->flame_reading = input;}
     void setFlameBool(bool value){flame_bool = value;}
     void setPin(const int pin){FLAME_SENSOR_PIN = pin;}
 
     Flame(){};
-    Flame(const int pin, int reading, bool flame){this->FLAME_SENSOR_PIN = pin;this->flame_reading = reading;this->flame_bool = flame;}
     Flame(const int pin){this->FLAME_SENSOR_PIN = pin;}
-    ~Flame(){ cout << "deleted";}
+    ~Flame(){};
 
 private:
     bool flame_bool = false;
@@ -46,15 +41,20 @@ public:
     void addFlame(int flame_pin);
     Flame* getArray(){return flame_array;}
 
-    string read();
+    char* read();
     void setIndex(int x){index = x;}
     int getArraySize(){return flame_array_size;};
 
 private:
+    char command[4];
     bool flame_sun = false;
+
+    char sun[4]{'f', '1', '0', '0'};
+    char* flame_status;
 
     int average_value = 500;
     int index = 0;
+    int amountOfSensors;
 
     const int MAX_READING = 650;
     const int FLAME_SENSITIVITY = 200;
@@ -63,7 +63,7 @@ private:
 
     Flame flame_array[5];
     int flame_array_size = 5;
-    string read_flame_array();
+    char* read_flame_array();
 };
 
 #endif //FLAME_FLAME_H
