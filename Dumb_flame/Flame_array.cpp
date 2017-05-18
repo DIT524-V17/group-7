151dd2ec6eb1a2cf8bdff8a6ae6eb5c1fe15f88c
Tim Jonasson <gusjonati@student.gu.se>
2017-05-18 16:29:26 +0200

    Added comments

--- Dumb_flame/Flame_array.cpp
+++ Dumb_flame/Flame_array.cpp
@@ -4,21 +4,25 @@
 
 #include "Flame_array.h"
 
+//sets the pin of the flame pin and puts it at the first index of the array
 Flame_array::Flame_array(unsigned short flame_pin) {
     flame_array[0].setPin(flame_pin);
     amountOfSensors = 1;
 }
+//sets the pin of the flame pins and puts them into the array
 Flame_array::Flame_array(unsigned short flame_pin, unsigned short flame_pin_2) {
     flame_array[0].setPin(flame_pin);
     flame_array[1].setPin(flame_pin_2);
     amountOfSensors = 2;
 }
+//sets the pin of the flame pins and puts them into the array
 Flame_array::Flame_array(unsigned short flame_pin, unsigned short flame_pin_2, unsigned short flame_pin_3) {
     flame_array[0].setPin(flame_pin);
     flame_array[1].setPin(flame_pin_2);
     flame_array[2].setPin(flame_pin_3);
     amountOfSensors = 3;
 }
+//sets the pin of the flame pins and puts them into the array
 Flame_array::Flame_array(unsigned short flame_pin, unsigned short flame_pin_2, unsigned short flame_pin_3, unsigned short flame_pin_4) {
     flame_array[0].setPin(flame_pin);
     flame_array[1].setPin(flame_pin_2);
@@ -26,6 +30,7 @@ Flame_array::Flame_array(unsigned short flame_pin, unsigned short flame_pin_2, u
     flame_array[3].setPin(flame_pin_4);
     amountOfSensors = 4;
 }
+//sets the pin of the flame pins and puts them into the array
 Flame_array::Flame_array(unsigned short flame_pin, unsigned short flame_pin_2, unsigned short flame_pin_3, unsigned short flame_pin_4, unsigned short flame_pin_5) {
     flame_array[0].setPin(flame_pin);
     flame_array[1].setPin(flame_pin_2);
@@ -34,55 +39,77 @@ Flame_array::Flame_array(unsigned short flame_pin, unsigned short flame_pin_2, u
     flame_array[4].setPin(flame_pin_5);
     amountOfSensors = 5;
 }
-
+//Returns a pointer to the char array that is the output of the library
 char* Flame_array::read_flame_array() {
+
+    //Reads the current value of the flame sensor
     (*(flame_array + index)).setFlame();
 
-    (*(flame_array + index)).getFlameValue() >= MAX_READING ? flame_sun = true : flame_sun = false;
+    //Checks if the reading could be the sun
+    flame_sun = (*(flame_array + index)).getFlameValue() >= MAX_READING;
     if(!flame_sun) {
+        //Starts creating the array
         command[0] = 'f';
         command[1] = '0';
-        command[2] =  '0' + index;
+        command[2] = '0' + index;
+
+        //Gets the value of the flame sensor
         int flame_value = (*(flame_array + index)).getFlameValue();
+        //Compares the reading to the readings of the other flame sensors
         if (compareToAverage(flame_value)) {
+            //If its a flame it sets the flame bool to true and returns a char array that says it sees a flame
             (*(flame_array + index)).setFlameBool(true);
             command[3] = '1';
             return command;
         }
+        //If it isnt a flame it sets the flame bool to false and then returns a char array that says that it doesnt see flame
         (*(flame_array + index)).setFlameBool(false);
         command[3] = '0';
         return command;
     }
+    //If the reading might be from sunlight then this will be returned
     return sun;
 }
 
+//Compares the value with the values of the rest of the flame sensors and returns true if its at least bigger than the average
+//value + the flame sensitivity
 bool Flame_array::compareToAverage(int &value){
     average_value = 0;
     int tempValue = value;
     int i = 0;
+    //Loops through all the sensors and saves the values
     for(i;i < amountOfSensors; i++){
         average_value += flame_array[i].getFlameValue();
     }
+    //Calculates the average value of the other sensors
     if(amountOfSensors > 1) {
         average_value -= tempValue;
         average_value = average_value / (amountOfSensors - 1);
 
+        //checks if the value is bigger than the average value + the flame sensitivity
         if (tempValue > FLAME_SENSITIVITY + average_value) {
             return true;
         }
+        //Checks if the sensor sees a flame when its only one sensor
     } else if (tempValue > FLAME_SENSITIVITY && tempValue < MAX_READING) {
         return true;
     }
     return false;
 }
 
+//Reads all the sensors and then returns their status
 char* Flame_array::read() {
+    //Checks if the next index in the char array contains an active flame sensor
     if (flame_array[index].getPin() == 0 || index == 5){
         this->index = 0;
     }
+    //reads the current flame sensor
     flame_status = read_flame_array();
+    //moves the index to the next sensor if the sensor is seeing something that isnt the sun
+    //the array {'f', '1', '0', ''0} is sent if think that its the sun
     if(flame_status[1] != '1') {
         this->index++;
     }
+    //returns the array
     return flame_status;
 }
--- Dumb_flame/Flame_array.h
+++ Dumb_flame/Flame_array.h
@@ -9,21 +9,32 @@
 
 class Flame{
 public:
+    //Returns true when the flame sensor sees a flame and false when it doesnt
     bool getFlameBool(){return flame_bool;}
+    //Returns the current value of the output from the flame sensor
     int  getFlameValue(){return flame_reading;}
+    //Returns the pin of the flame sensor
     int  getPin(){return FLAME_SENSOR_PIN;}
+
+    //Reads from the flame sensor and updates the current value of the output
     void setFlame(){flame_reading = analogRead(FLAME_SENSOR_PIN);}
-    void setFlame(int input){this->flame_reading = input;}
+    //Sets the bool value of the flame sensor to the inputed value
     void setFlameBool(bool value){flame_bool = value;}
+    //Sets the pin of the flame sensor
     void setPin(const int pin){FLAME_SENSOR_PIN = pin;}
 
     Flame(){};
     Flame(const int pin){this->FLAME_SENSOR_PIN = pin;}
+    //Destructor
     ~Flame(){};
 
 private:
+    //False for when it isn't seeing a flame
+    //True when it sees a flame
     bool flame_bool = false;
+    //The saved output from the flame sensor between approximately 0 and 1000
     int flame_reading = 0;
+    //The pin number
     int FLAME_SENSOR_PIN = 0;
 };
 
@@ -37,31 +48,37 @@ public:
     Flame_array(unsigned short flame_pin, unsigned short flame_pin_2, unsigned short flame_pin_3, unsigned short flame_pin_4, unsigned short flame_pin_5);
     ~Flame_array(){};
 
-    void removeFlame();
-    void addFlame(int flame_pin);
+    //Returns a pointer to the output from the flame sensors
+    char* read();
+    //Returns the array containing the flame sensors
     Flame* getArray(){return flame_array;}
 
-    char* read();
-    void setIndex(int x){index = x;}
-    int getArraySize(){return flame_array_size;};
-
 private:
+    //The char array that is going to contain the output from the library
     char command[4];
+    //Is true while a flame sensor thinks it sees the sun
     bool flame_sun = false;
 
+    //The output for when the flame sensor sees the sun
     char sun[4]{'f', '1', '0', '0'};
+    //A variable for saving the pointer to the char array that is the output from the read() method
     char* flame_status;
 
+    //Initialized the values to this
     int average_value = 500;
     int index = 0;
     int amountOfSensors;
 
+    //Every reading higher than this will be interpreted as the sun
     const int MAX_READING = 650;
+    //The number that is used to decide whether the flame sensor sees a flame or not
     const int FLAME_SENSITIVITY = 200;
-
+    //Compares the inputed value to the average of all the other flame sensors
     bool compareToAverage(int &value);
 
+    //The array of flame sensors
     Flame flame_array[5];
+    //The size of the array
     int flame_array_size = 5;
     char* read_flame_array();
 };
