a071d2f0c3ff47627b5fc530dacd798a11306872
Sebastian-Fransson <gusfransse@student.gu.se>
2017-05-24 14:36:01 +0200

    Changes to the receiver

--- tcpclient/receiver.py
+++ tcpclient/receiver.py
@@ -12,13 +12,14 @@ import io
 import serial
 
 # The encoding and decoding procedure to follow.
-coding = 'utf-8'  # 'ISO8859-1'
+coding = 'utf-8' # 'ISO8859-1'
 
 # Arduino connected via a specific USB port.
 usb = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
 
 
 class Receiver:
+
     receiver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = None
     port = None
@@ -28,6 +29,7 @@ class Receiver:
     prv = 0
     b = None
     client = None
+    
 
     # Constructor
     def __init__(self, host, port):
@@ -52,6 +54,7 @@ class Receiver:
         # The server can listen up to 5 backlog connections.
         self.receiver.listen(5)
 
+
         # Initalize read and write loop.
         self.reconnect()
 
@@ -63,16 +66,16 @@ class Receiver:
             print("Address: {}".format(self.host))
             print("Port: {}".format(self.port))
             print("0. Awaiting connection.")
-
+   
             # Grabs the tuples for the socket and address of the client.
             (client, address) = self.receiver.accept()
             print("1. Client connected.")
 
             # Runs the image capturing in a seperate thread.
-
+            
             threading.Thread(target=self.drawme).start()
-            # threading.Thread(target=self.bgFrame).start()
-
+            #threading.Thread(target=self.bgFrame).start()
+            
             # Make the new opened socket use a none-blocking flag, so it can read and write.
             client.setblocking(0)
             self.client = client
@@ -90,22 +93,25 @@ class Receiver:
 
                     # Times out according to previous declaration.
                     if ready[0]:
+
                         # Able to receive up to 4096 bytes in one package.
                         self.msg = client.recv(40000)
 
                     # If there does not exist a message due to a connection issue, End loop.
-
+			
 
                     if self.msg:
-                        # Decodes the message received from bytes to text using either utf or ascii.
-                        self.msg = self.msg.decode(coding)
-                        print("2. To Arduino: " + self.msg)
 
-                        # Writes the message to the serial port on the Arduino.
-                        usb.write(self.msg.encode(coding))
+			
+                            # Decodes the message received from bytes to text using either utf or ascii.
+                            #self.msg = self.msg.decode(coding)
+                            print("2. To Arduino: ")
 
-                        # Flush the stream to force it to write to the buffer.
-                        usb.flush()
+                            # Writes the message to the serial port on the Arduino.
+                            usb.write(self.msg)
+
+                            # Flush the stream to force it to write to the buffer.
+                            usb.flush()
 
                     # Found this clever solution here:
                     # http://stackoverflow.com/questions/38645060/what-is-the-equivalent-of-serial-available-in-pyserial
@@ -113,13 +119,12 @@ class Receiver:
                     while usb.inWaiting():
 
                         # Grabs an entire line (until a newline character exists). And decode in to readable text.
-
                         try:
                             pass
-                            # info = usb.readline().decode()
-                            # print("3. To Android: " + info)
-                            # client.send(info.encode(coding))
-                        except:
+                            info = usb.readline().decode()
+                            print("3. To Android: " + info)
+                       	    # client.send(info.encode(coding))
+                       	except:
                             print("Unable to read")
                             # Send the message to the client.
 
@@ -136,70 +141,82 @@ class Receiver:
         print("Disconnected at: %s" % s)
 
     def drawme(self):
-        # stream = io.BytesIO()
-        # imgsent = 0
+        #stream = io.BytesIO()
+        #imgsent = 0
         with picamera.PiCamera() as camera:
-            camera.resolution = (640, 480)  # Sets the resolution to 640x480.
+            res_width = 1280
+            res_height = 720
+            camera.resolution = (res_width, res_height)  # Sets the resolution to 640x480.
+            # camera.zoom = (0, 0, 0.2, 0.2)
+            
+            time.sleep(2) # Let the camera warm up for 2 seconds.
 
-            time.sleep(3)
-            # Loops through indefinetly for capturing sequence
-
-            while True:
-                if not self.connection:  # Check if a connection is available
-                    break
-                camera.capture('img.jpg', use_video_port=True)
-
-                with open('img.jpg', "rb") as imageFile:
-
-                    f = imageFile.read()
-                    self.b = bytearray(f)
-
-                    bc = len(self.b)
-                    if bc < 20000:
-                        continue
-                    if bc < 1:
-                        continue
-                    if self.prv == bc:
-                        continue
-                    self.prv = bc
-
-                    # Start a new thread where the sending of frames will be done
+            #Loops through indefinetly for capturing sequence
+            while self.connection:
+                camera.capture('img.jpg', resize=(int(res_width/3), int(res_height/3)),use_video_port=True)
+                with open('img.jpg',"rb") as imageFile:
+                    self.b = bytearray(imageFile.read())
+                    
+                    # Start a new thread where the sending of frames will be done          
                     try:
-
-                        threading.Thread(target=self.sendFrame).start()
-                        time.sleep(3)  # To be replaced with frame rate delay
+                        threading.Thread(target=self.sendFrame, args=(self.b,)).start()
+                        # time.sleep(0.05)      
                     except:
                         traceback.print_exc()
-                        break  # set the camera object as an instance of the picamera lib
-
+                        self.connection = False
+                        break # set the camera object as an instance of the picamera lib
+    
     # Method for sending the frames through the Java Client
     # Params:
     # b is the byte array
-    # client is the socket
-    def sendFrame(self):
+    # client is the socket                                                                                                                            
+    def sendFrame(self, b):
         try:
-
-            # l = (self.b[0], self.b[1], self.b[2], self.b[3])
-            # print("Printing as INT:" + str(int.from_bytes(l, byteorder='big')))
             index = 1
-            div = 1024
-            for i in range(int(len(self.b) / div)):
-                self.client.send(self.b[(index - 1) * div:index * div])
-                index += 1
+            div = 1024*8
+            # for i in range (int(len(b)/div)):
+                # threading.Thread(target=self.sendInBG, args= (b[index-1*div:index*div],)).start()
+                # self.sendInBG(b[(index-1)*div:index*div])
+                # index += 1
+                # time.sleep(1/int(len(b)/div)) # Reduces unavailable resources issue.
 
-                time.sleep(0.08)
-            # print("Video Length: {}".format(len(b)))
-            self.client.send(self.b[index * div::])
-            print(len(self.b))
+            # Sends the remaining bytes which is smaller than a package.
+            # self.sendInBG(b[(index-1)*div::])
+            self.sendInBG(b)
+            # threading.Thread(target=self.sendInBG, args= (b[index*div::],)).start()
+            # threading.Thread(target=self.sendInBG, args= (b[index*div::],)).start()
+
+            print("Frame sent with length: {}".format(len(b)) )
         except:
             traceback.print_exc()
             return
 
-    # Initializes and runs the capture sequence.
-    # Recommended to be performed in a seperate thread.
-    def bgFrame(self):
-        time.sleep(2)
-        while True:
-            time.sleep(1 / 24)
-            if not self.connection:  # Check if a connection is available
-                break
+    
+    def sendInBG(self, b):
+        try:
+            self.client.send(b)
+        except BlockingIOError:
+            time.sleep(0.05)
+            self.client.send(b)
+        except:
+            traceback.print_exc()
+            self.connection = False
+        
+        
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
