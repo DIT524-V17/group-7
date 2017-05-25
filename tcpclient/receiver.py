0266f8b163d14668386265aa1c149078466ced06
Sebastian-Fransson <gusfransse@student.gu.se>
2017-05-25 20:19:35 +0200

    Multi-Socket receiver

--- tcpclient/receiver.py
+++ tcpclient/receiver.py
@@ -15,12 +15,13 @@ import serial
 coding = 'utf-8' # 'ISO8859-1'
 
 # Arduino connected via a specific USB port.
-usb = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
+usb = serial.Serial('/dev/ttyACM0', 9600, timeout=.2)
 
 
 class Receiver:
 
-    receiver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    command_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    camera_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = None
     port = None
     connection = False
@@ -29,6 +30,9 @@ class Receiver:
     prv = 0
     b = None
     client = None
+    camera_feed = None
+    fps = time.time()
+    ite = 0
     
 
     # Constructor
@@ -48,11 +52,13 @@ class Receiver:
         # the python script will be a more seemless experience.
         os.system("sudo fuser -k {}/tcp".format(self.port))
 
-        # Binds the server to the socket.
-        self.receiver.bind((self.host, self.port))
+        # Binds the server to the sockets.
+        self.command_sock.bind((self.host, 9006))
+        self.camera_sock.bind((self.host, self.port))
 
         # The server can listen up to 5 backlog connections.
-        self.receiver.listen(5)
+        self.command_sock.listen(5)
+        self.camera_sock.listen(5)
 
 
         # Initalize read and write loop.
@@ -68,17 +74,15 @@ class Receiver:
             print("0. Awaiting connection.")
    
             # Grabs the tuples for the socket and address of the client.
-            (client, address) = self.receiver.accept()
+            (self.camera_feed, address) = self.camera_sock.accept()
+            (self.client, address) = self.command_sock.accept()
             print("1. Client connected.")
 
             # Runs the image capturing in a seperate thread.
-            
             threading.Thread(target=self.drawme).start()
-            #threading.Thread(target=self.bgFrame).start()
             
             # Make the new opened socket use a none-blocking flag, so it can read and write.
-            client.setblocking(0)
-            self.client = client
+            self.client.setblocking(0)
             # Used to decide if the client is able to reconnect or not.
             self.connection = True
 
@@ -89,13 +93,13 @@ class Receiver:
                     # This follows this example of how to use select in python:
                     # http://stackoverflow.com/questions/2719017/how-to-set-timeout-on-pythons-socket-recv-method
                     # This will give me a none blocking message receiver.
-                    ready = select.select([client], [], [], 0.1)
+                    ready = select.select([self.client], [], [], 0.2)
 
                     # Times out according to previous declaration.
                     if ready[0]:
 
                         # Able to receive up to 4096 bytes in one package.
-                        self.msg = client.recv(40000)
+                        self.msg = self.client.recv(40000)
 
                     # If there does not exist a message due to a connection issue, End loop.
 			
@@ -123,7 +127,7 @@ class Receiver:
                             pass
                             info = usb.readline().decode()
                             print("3. To Android: " + info)
-                       	    # client.send(info.encode(coding))
+                       	    self.client.send(info.encode(coding))
                        	except:
                             print("Unable to read")
                             # Send the message to the client.
@@ -140,66 +144,73 @@ class Receiver:
     def disconnected(s):
         print("Disconnected at: %s" % s)
 
+    def openImg(self):
+        with open('img.jpg',"rb") as imageFile:
+            self.b = bytearray(imageFile.read()) # Create a byte array of the saved image file.
+            threading.Thread(target=self.sendFrame, args=(self.b,)).start()
+
+
     def drawme(self):
-        #stream = io.BytesIO()
-        #imgsent = 0
+        # Sets the camera object and resolution.
         with picamera.PiCamera() as camera:
             res_width = 1280
             res_height = 720
-            camera.resolution = (res_width, res_height)  # Sets the resolution to 640x480.
-            # camera.zoom = (0, 0, 0.2, 0.2)
+            camera.resolution = (res_width, res_height)  # Sets the resolution to 1280x720.
+            camera.brightness = 45
             
             time.sleep(2) # Let the camera warm up for 2 seconds.
 
             #Loops through indefinetly for capturing sequence
             while self.connection:
-                camera.capture('img.jpg', resize=(int(res_width/3), int(res_height/3)),use_video_port=True)
-                with open('img.jpg',"rb") as imageFile:
-                    self.b = bytearray(imageFile.read())
+                # Capture an image and rezize it to 426x240.
+                # Set the video port to True for an increase in sending speed while sacrificing quality.
+                self.ite += 1
+                if (int(time.time()) >= self.fps+2):
+                    print("FPS: {}".format(self.ite/2))
+                    self.fps = int(time.time())
+                    self.ite = 0
                     
-                    # Start a new thread where the sending of frames will be done          
-                    try:
-                        threading.Thread(target=self.sendFrame, args=(self.b,)).start()
-                        # time.sleep(0.05)      
-                    except:
-                        traceback.print_exc()
-                        self.connection = False
-                        break # set the camera object as an instance of the picamera lib
+                
+                camera.capture('img.jpg', resize=(int(res_width/3), int(res_height/3)),use_video_port=True)
+                threading.Thread(target=self.openImg).start()
+                
     
     # Method for sending the frames through the Java Client
-    # Params:
     # b is the byte array
-    # client is the socket                                                                                                                            
+    # camera_feed is the used socket                                                                                                                            
     def sendFrame(self, b):
         try:
             index = 1
             div = 1024*8
-            # for i in range (int(len(b)/div)):
-                # threading.Thread(target=self.sendInBG, args= (b[index-1*div:index*div],)).start()
-                # self.sendInBG(b[(index-1)*div:index*div])
-                # index += 1
-                # time.sleep(1/int(len(b)/div)) # Reduces unavailable resources issue.
+            for i in range (int(len(b)/div)):
+                #threading.Thread(target=self.sendInBG, args= (b[index-1*div:index*div],)).start()
+                self.sendInBG(b[(index-1)*div:index*div])
+                index += 1
+                #time.sleep(1/int(len(b)/div)) # Reduces unavailable resources issue.
 
             # Sends the remaining bytes which is smaller than a package.
-            # self.sendInBG(b[(index-1)*div::])
-            self.sendInBG(b)
-            # threading.Thread(target=self.sendInBG, args= (b[index*div::],)).start()
-            # threading.Thread(target=self.sendInBG, args= (b[index*div::],)).start()
+            self.sendInBG(b[(index-1)*div::])
+            #threading.Thread(target=self.sendInBG, args= (b[index*div::],)).start()
+            #threading.Thread(target=self.sendInBG, args= (b[index*div::],)).start()
 
             print("Frame sent with length: {}".format(len(b)) )
         except:
             traceback.print_exc()
             return
 
-    
+    # Send the images and check for occurrances of IOErrors.
     def sendInBG(self, b):
         try:
-            self.client.send(b)
-        except BlockingIOError:
-            time.sleep(0.05)
-            self.client.send(b)
+            self.camera_feed.send(b) # Sends the image through the socket to the Java client.
+            #self.camera_feed.send(b[int(len(b)/2):len(b)]) # Sends the image through the socket to the Java client.
+
+        # Catch error and try to resend the image.
+        except BlockingIOError: 
+            time.sleep(0.1)
+            self.camera_feed.send(b)
         except:
             traceback.print_exc()
+
             self.connection = False
         
         
