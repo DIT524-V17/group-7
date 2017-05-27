fda1fa20fe3f4cdb437c80b0d8bb8f71e71deee1
Sebastian-Fransson <gusfransse@student.gu.se>
2017-05-27 11:26:11 +0200

    Removed unused code, added comments, added version

--- TCP raspberry/receiver.py
+++ TCP raspberry/receiver.py
@@ -11,8 +11,10 @@ import io
 # This import will throw a syntax error if the module is not installed.
 import serial
 
+# @ Version 5.4.1
+
 # The encoding and decoding procedure to follow.
-coding = 'utf-8' # 'ISO8859-1'
+coding = 'utf-8' # 'ISO8859-1' if encoding is an issue try this.
 
 # Arduino connected via a specific USB port.
 usb = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
@@ -20,6 +22,7 @@ usb = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
 
 class Receiver:
 
+    
     command_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     camera_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = None
@@ -74,7 +77,7 @@ class Receiver:
             print("Port: {}".format(self.port))
             print("0. Awaiting connection.")
    
-            # Grabs the tuples for the socket and address of the client.
+            # Grabs the tuples for the sockets and address of the client and camera_feed.
             (self.camera_feed, address) = self.camera_sock.accept()
             (self.client, address) = self.command_sock.accept()
             print("1. Client connected.")
@@ -107,13 +110,9 @@ class Receiver:
 
                     if self.msg:
 
-			
-                            # Decodes the message received from bytes to text using either utf or ascii.
-                            # self.msg = self.msg.decode(coding)
-
                             # Writes the message to the serial port on the Arduino.
                             if (self.msg_last != self.msg):
-                                # print("2. To Arduino: ") 
+                                print("2. To Arduino: ") 
                                 usb.write(self.msg)
                                 self.msg_last = self.msg
 
@@ -155,49 +154,36 @@ class Receiver:
     def drawme(self):
         # Sets the camera object and resolution.
         with picamera.PiCamera() as camera:
-            res_width = 1280
-            res_height = 720
             camera.framerate = 10
-            camera.resolution = (320, 191)  # Sets the resolution to 1280x720.
+            camera.resolution = (320, 191)  # Sets the resolution to 320x191.
             camera.brightness = 45
             
             time.sleep(2) # Let the camera warm up for 2 seconds.
 
             #Loops through indefinetly for capturing sequence
             while self.connection:
-                # Capture an image and rezize it to 426x240.
+                # Capture an image
                 # Set the video port to True for an increase in sending speed while sacrificing quality.
-                # resize=(int(1280/4), int(720/4)),
                 camera.capture('img.jpg', use_video_port=True)
-                # self.ite += 1
-                # if (int(time.time()) >= self.fps+2):
-                    # print("FPS: {}".format(self.ite/2))
-                    # self.fps = int(time.time())
-                    # self.ite = 0
      
                 threading.Thread(target=self.openImg).start()
                 
             
     
-    # Method for sending the frames through the Java Client
-    # b is the byte array
-    # camera_feed is the used socket                                                                                                                            
+    # Method for sending the frames through the Java Client.
+    # b is the byte array.
+    # camera_feed is the used socket .                                                                                                                           
     def sendFrame(self, b):
         try:
             index = 1
             div = 1024*8
+            # Chop up the image and send it in smaller packages.
             for i in range (int(len(b)/div)):
-                #threading.Thread(target=self.sendInBG, args= (b[index-1*div:index*div],)).start()
                 self.sendInBG(b[(index-1)*div:index*div])
                 index += 1
-                #time.sleep(1/int(len(b)/div)) # Reduces unavailable resources issue.
 
             # Sends the remaining bytes which is smaller than a package.
             self.sendInBG(b[(index-1)*div::])
-            #threading.Thread(target=self.sendInBG, args= (b[index*div::],)).start()
-            #threading.Thread(target=self.sendInBG, args= (b[index*div::],)).start()
-
-           # print("Frame sent with length: {}".format(len(b)) )
         except:
             traceback.print_exc()
             return
@@ -206,15 +192,13 @@ class Receiver:
     def sendInBG(self, b):
         try:
             self.camera_feed.send(b) # Sends the image through the socket to the Java client.
-            #self.camera_feed.send(b[int(len(b)/2):len(b)]) # Sends the image through the socket to the Java client.
         # Catch error and try to resend the image.
         except BlockingIOError: 
             time.sleep(0.1)
             self.camera_feed.send(b)
         except:
             traceback.print_exc()
-
-            self.connection = False
+            self.connection = False # Closes connection.
         
         
             
