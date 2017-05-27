87949ac485fd403cf05923017adc191dc17af5d5
Pontus <guslaepo@student.gu.se>
2017-05-27 12:13:32 +0200

    Removed OLD TCP

--- tcpclient/server.py
+++
@@ -1,9 +0,0 @@
-# Author: Pontus Laestadius.
-# Since: 2nd of March, 2017.
-# Maintained since: 17th of April 2017.
-
-from receiver import Receiver
-print("Version 2.2")
-Receiver("172.24.1.1", 9005)
-
-
